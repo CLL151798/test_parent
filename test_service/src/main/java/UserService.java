1efd87c90b8783e375a3f9bd53ab8fcbb37feb44
sgyx <sgyx_151798@163.com>
2017-07-28 10:02:33 +0800

    测试提交................1

---
+++ test_service/src/main/java/UserService.java
@@ -0,0 +1,8 @@
+/**
+ * Created by sgyx on 2017/7/26 0026.
+ */
+public class UserService {
+    public static void sayHello(){
+        System.out.println("..........");
+    }
+}
---
+++ test_web/src/main/java/UserAction.java
@@ -0,0 +1,9 @@
+/**
+ * Created by sgyx on 2017/7/26 0026.
+ */
+public class UserAction {
+    public static void main(String[] args) {UserService.sayHello();
+        System.out.println("呵呵呵呵呵呵");
+    }
+
+}
